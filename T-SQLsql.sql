2937dfcbeb322191c57f9f6ce24a1a2f949e8014
bigsmokio69 <jecs1704cs@gmail.com>
2024-06-19 18:20:06 -0600

    Scripts terminados de la practica 10, añadidos case y una consulta

--- T-SQLsql.sql
+++ T-SQLsql.sql
@@ -34,7 +34,7 @@ ElSE
   BEGIN
 		print @contador
 		set @contador=@contador+1
-  END */
+  END 
 --Return
  declare @contador int=0
  WHILE @contador <=10
@@ -55,3 +55,21 @@ END CATCH
 
   print ('soy otra consulta')
   print('yo tambien')
+*/
+
+--CASE
+declare @valor int
+declare @resultado char(10)=''
+set @valor=20
+
+set @resultado=(CASE WHEN @valor=10 THEN 'ROJO'
+				WHEN @valor=20 THEN 'MORADO'
+				WHEN @valor=30 THEN 'FUXIA'
+				ELSE 'GRIS' 
+				END)
+print @resultado
+
+SELECT * ,(CASE WHEN disponibilidad=1 THEN 'VERDE'
+				WHEN disponibilidad=0 THEN 'ROJO'
+				ELSE 'NEGRO' END) AS INDICADOR FROM Inventario
+
